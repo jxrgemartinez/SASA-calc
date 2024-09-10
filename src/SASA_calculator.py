33b699131b44277fb0cacae7c946e6a8a59f0304
jxrgemartinez <jxrgemartinez@icloud.com>
2024-09-10 20:27:49 +0200

    try paralleling

--- src/SASA_calculator.py
+++ src/SASA_calculator.py
@@ -6,7 +6,7 @@ def run_calculator(protein):
     return calculator
 
 if __name__ == '__main__':
-    protein = classes.Protein("/Users/jorge/Library/Mobile Documents/com~apple~CloudDocs/Downloads/1a5d.pdb")
+    protein = classes.Protein("data/1l2y.pdb")
     total_sasa = run_calculator(protein)
     
     total_sasa.print_sasa_report(output_type="residue")
