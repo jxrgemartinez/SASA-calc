d876ccf59a706c34fa1016fcf859c2abd323347e
jxrgemartinez <jxrgemartinez@icloud.com>
2024-09-12 11:50:08 +0200

    added pdb files with which tests have been done

--- src/classes_noP.py
+++ src/classes_noP.py
@@ -1,82 +1,53 @@
 import os
 from Bio.PDB import PDBParser
 import numpy as np
-from multiprocessing import Pool
 
 ATOM_VdW = {
-    "H": 1.200,
-    "HE": 1.400,
-    "C": 1.700,
-    "N": 1.550,
-    "O": 1.520,
-    "F": 1.470,
-    "NA": 2.270,
-    "MG": 1.730,
-    "P": 1.800,
-    "S": 1.800,
-    "CL": 1.750,
-    "K": 2.750,
-    "CA": 2.310,
-    "NI": 1.630,
-    "CU": 1.400,
-    "ZN": 1.390,
-    "SE": 1.900,
-    "BR": 1.850,
-    "CD": 1.580,
-    "I": 1.980,
-    "HG": 1.550,
-}
-
+        "H": 1.200,
+        "HE": 1.400,
+        "C": 1.700,
+        "N": 1.550,
+        "O": 1.520,
+        "F": 1.470,
+        "NA": 2.270,
+        "MG": 1.730,
+        "P": 1.800,
+        "S": 1.800,
+        "CL": 1.750,
+        "K": 2.750,
+        "CA": 2.310,
+        "NI": 1.630,
+        "CU": 1.400,
+        "ZN": 1.390,
+        "SE": 1.900,
+        "BR": 1.850,
+        "CD": 1.580,
+        "I": 1.980,
+        "HG": 1.550,
+    }
 
 class Atom:
-    """
-    Represents an atom within the molecular structure, containing
-    properties relevant to both the atom itself and its context
-    within a larger molecule
-    """
     def __init__(self, serial, name, atom_type, aa_name, aa_number, chain_id, coord):
-        """
-        Arguments:
-        - serial (int): Atom number.
-        - name (str): Atom name.
-        - atom_type (str): Element type.
-        - aa_name (str): Amino acid name.
-        - aa_number (int): Amino acid sequence number.
-        - chain_id (str): Chain identifier.
-        - coord (list): 3D coordinates of the atom as a list [x, y, z].
-        """
-        self.serial = serial 
-        self.name = name
-        self.type = atom_type
-        self.aa_name = aa_name
-        self.aa_number = aa_number
-        self.chain_id = chain_id
-        self.coord = np.array(coord)
-        self.rad_vdw = ATOM_VdW.get(atom_type, 1.0)
+        self.serial = serial  # Atom number 
+        self.name = name  # Atom name
+        self.type = atom_type  # Element type
+        self.aa_name = aa_name  # Amino acid name
+        self.aa_number = aa_number  # Amino acid sequence number
+        self.chain_id = chain_id  # Chain
+        self.coord = np.array(coord)  # Coordinates
+        self.rad_vdw = ATOM_VdW.get(atom_type, 1.0) # Van der Waals radius
         self.neighbors = []
-        self.abs_sasa = 0
-        self.rel_sasa = 0
-    
-    def add_neighbor(self, other):
-        """
-        Adds another atom to the list of this atom's neighbors.
-        """
-        self.neighbors.append(other)
- 
- 
-def distance_pair(coords):
-    """
-    Calculates the distance between two points
-    """
-    coord1, coord2 = coords
-    return np.linalg.norm(coord1 - coord2)
+        self.abs_sasa = 0 # Absolute solvent accessible surface area
+        self.rel_sasa = 0 # Relative solvent accessible surface area
 
-   
+    def calc_distance(self, other): # Calculate distance between atoms
+        return np.linalg.norm(self.coord - other.coord)
+
+    def add_neighbor(self, other):
+        self.neighbors.append(other)
+    
+    
 class Protein:
-    """
-    Represents the protein structure: it parses a PDB file,
-    and computes the distances between all the protein atoms.
-    """
     def __init__(self, filepath, model_index=0):
         if not os.path.exists(filepath):
             raise FileNotFoundError(f"File does not exist: {filepath}")
@@ -84,8 +55,7 @@ class Protein:
         self.list_atoms = []
         self.atom_index_map = {}
         self._build_mlc_from_pdb(filepath, model_index)
-        
-        self.distance_matrix = self.compute_distance_matrix()
+        self.distance_matrix = self.calculate_distances()
 
     def add_atom(self, atom):
         if isinstance(atom, Atom):
@@ -93,32 +63,27 @@ class Protein:
             self.atom_index_map[atom.serial] = len(self.list_atoms) - 1
     
     def _build_mlc_from_pdb(self, filepath, model_index):
-        """
-        Parse the PDB file and store the atoms coordinates.
-
-        Arguments:
-        filepath (str): Path to the PDB file.
-        model_index (int): Index of the model to parse.
-        """
         parser = PDBParser(QUIET=True)
         structure_id = filepath.split('/')[-1].split('.')[0]
         
+        # Load structure
         structure = parser.get_structure(structure_id, filepath)
         
-        # Check if model index is valid
-        if model_index >= len(structure):
+        # Check if the model_index is valid
+        try:
+            model = structure[model_index]
+        except IndexError:
             raise IndexError(f"Model index {model_index} is out of bounds. Structure only has {len(structure)} models.")
-        model = structure[model_index]
         
         for chain in model:
             for residue in chain:
-                if residue.get_resname() == 'HOH' or residue.get_resname() == 'HETATM' or residue.get_resname() == 'WAT':
-                    continue  # Skip water molecules and heteroatoms
+                if residue.get_resname() == 'HOH':
+                    continue  # Skip water molecules
                 
-                aa_name = residue.resname
-                aa_number = residue.get_id()[1]
+                aa_name = residue.resname  # Get the amino acid name
+                aa_number = residue.get_id()[1]  # Get the sequence number of the amino acid
                 
-                chain_id = chain.id
+                chain_id = chain.id  # Get chain
                 
                 for atom in residue.get_atoms():
                     serial = atom.serial_number
@@ -128,53 +93,33 @@ class Protein:
                     
                     new_atom = Atom(serial, name, atom_type, aa_name, aa_number, chain_id, coord)
                     self.add_atom(new_atom)
-   
-    def compute_distance_matrix(self):
-        """
-        Computes the distance matrix for all atoms in the protein.
-        """
-        num_atoms = len(self.list_atoms)
-        tasks = [(self.list_atoms[i].coord, self.list_atoms[j].coord) for i in range(num_atoms) for j in range(i, num_atoms)]
 
-        with Pool() as pool:
-            results = pool.map(distance_pair, tasks)
-
-        distance_matrix = np.zeros((num_atoms, num_atoms))
-        result_index = 0
-        for i in range(num_atoms):
-            for j in range(i, num_atoms):
-                distance = results[result_index]
-                distance_matrix[i][j] = distance
-                distance_matrix[j][i] = distance
-                result_index += 1
-
+    def calculate_distances(self): # Create a distance matrix for all atoms
+        n = len(self.list_atoms)
+        distance_matrix = np.zeros((n, n))
+        for i in range(n):
+            for j in range(i + 1, n):
+                dist = self.list_atoms[i].calc_distance(self.list_atoms[j])
+                distance_matrix[i][j] = dist
+                distance_matrix[j][i] = dist
         return distance_matrix
-
+    
     def determine_neighbors(self, atom_index, cutoff=15):
-        """
-        Determines close atoms for a given atom within a specified cutoff distance.
-        The cutoff was determined based on the radius of the largest Van der Waals radius
-        (K) and the sonde diameter (2*1.4) = 2.8 A.
-        """
+        # Take the row of each atom in the distance matrix and add coundt as neighbors the ones that are closer than the cutoff
         for j in range(len(self.list_atoms)):
             if j != atom_index and self.distance_matrix[atom_index][j] < cutoff:
                 self.list_atoms[atom_index].add_neighbor(self.list_atoms[j])
-
-
+   
+                             
 class Sphere:
-    """
-    Represents a geometric sphere used for calculating solvent accessible surface area (SASA).
-    This class generates a unit sphere with the Fibonacci lattice methodn and can relocate it
-    and rescale it based on the coordinates and van der Waals radius of a center atom.
-    """
     def __init__(self, n_points=100):
         self.n_points = n_points
         self.unit_sphere_points = self._generate_unit_sphere_points()
     
-    def _generate_unit_sphere_points(self):
+    def _generate_unit_sphere_points(self): # Sphere of radius 1 to reescale after
         points = []
-        inc = np.pi * (3 - np.sqrt(5))
-        offset = 2 / self.n_points
+        inc = np.pi * (3 - np.sqrt(5))  # Calculate the golden angle for point distribution
+        offset = 2 / self.n_points  # Determine vertical spacing between points
 
         for i in range(self.n_points):
             y = i * offset - 1 + (offset / 2)  
@@ -188,70 +133,40 @@ class Sphere:
         return np.array(points)
 
     def rescale_sphere(self, center_atom, sonde_radius):
+        # Rescale and relocate the sphere
         radius = center_atom.rad_vdw + sonde_radius
         adjusted_points = (self.unit_sphere_points * radius) + center_atom.coord
         
         return adjusted_points
- 
-    
-class SASACalculator:
-    """
-    Calculates the Solvent Accessible Surface Area (SASA) of a protein using a sphere-based
-    approximation method. This class handles the computation across all atoms in the protein
-    and provides detailed SASA reports.
-    """
-    def __init__(self, protein, n_points=100, sonde_radius=1.4):
-        """
-        Initializes a SASA calculator for a given protein.
 
-        Args:
-        protein (Protein): The protein for which SASA will be calculated.
-        n_points (int): Number of points on the sphere used in the calculation.
-        sonde_radius (float): Radius of the probe sphere used in SASA calculation.
-        """
+  
+class SASACalculator:
+    def __init__(self, protein, n_points=100, sonde_radius=1.4):
         self.protein = protein
         self.sphere = Sphere(n_points)
         self.sonde_radius = sonde_radius
-        self.protein.total_absolute_sasa = 0
-        
-    def compute_atom_sasa(self, atom):
-        """
-        Computes the SASA for a single atom.
-
-        Args:
-        atom (Atom): The atom for which SASA is being calculated.
-
-        Returns:
-        float: The solvent accessible surface area of the atom.
-        """
-        atom_index = self.protein.atom_index_map[atom.serial]
-        self.protein.determine_neighbors(atom_index)
-
-        rescaled_sphere_points = self.sphere.rescale_sphere(atom, self.sonde_radius)
-        accessible_points = 0
-
-        for point in rescaled_sphere_points:
-            if all(np.linalg.norm(point - neighbor.coord) >= (neighbor.rad_vdw + self.sonde_radius) for neighbor in atom.neighbors):
-                accessible_points += 1
-
-        sphere_area = 4 * np.pi * (atom.rad_vdw + self.sonde_radius) ** 2
-        accessible_area = (accessible_points / len(rescaled_sphere_points)) * sphere_area
-        return accessible_area
+        self.protein.total_absolute_sasa = 0  
 
     def run(self):
-        """
-        Executes the SASA calculation for all atoms in the protein and updates their SASA values.
-        """
-        with Pool() as pool:
-            results = pool.map(self.compute_atom_sasa, self.protein.list_atoms)
+        for atom in self.protein.list_atoms:
+            atom_index = self.protein.atom_index_map[atom.serial]
+            self.protein.determine_neighbors(atom_index)
 
-        total_absolute_sasa = sum(results)
-        for atom, abs_sasa in zip(self.protein.list_atoms, results):
-            atom.abs_sasa = abs_sasa
-            atom.rel_sasa = (abs_sasa / total_absolute_sasa * 100) if total_absolute_sasa > 0 else 0
-        
-        self.protein.total_absolute_sasa = total_absolute_sasa
-    
+            rescaled_sphere_points = self.sphere.rescale_sphere(atom, self.sonde_radius)
+            accessible_points = 0
+
+            for point in rescaled_sphere_points:
+                if all(np.linalg.norm(point - neighbor.coord) >= (neighbor.rad_vdw + self.sonde_radius) for neighbor in atom.neighbors):
+                    accessible_points += 1
+
+            sphere_area = 4 * np.pi * (atom.rad_vdw + self.sonde_radius) ** 2
+            accessible_area = (accessible_points / len(rescaled_sphere_points)) * sphere_area
+            atom.abs_sasa = accessible_area
+            self.protein.total_absolute_sasa += accessible_area  # Accumulate total SASA
+
+        for atom in self.protein.list_atoms:
+            atom.rel_sasa = (atom.abs_sasa / self.protein.total_absolute_sasa * 100) if self.protein.total_absolute_sasa > 0 else 0
+
     def print_sasa_report(self, output_type="copmlete"):
         """
         Prints a report of the SASA calculations.
@@ -260,7 +175,6 @@ class SASACalculator:
         output_type (str): Specifies the type of SASA report ('total', 'atomic', 'residue', 'complete').
         """
         possible_arguments = ["total", "atomic", "residue", "complete"]
-        
         if output_type not in possible_arguments:
             raise ValueError(f"Invalid output type. Possible types are {possible_arguments}")
         
@@ -269,8 +183,7 @@ class SASACalculator:
                 print("{:<5} {:>5} {:>4} {:>4} {:>4} {:>4} {:>8.2f} {:>8.2f}".format("ATOM", atom.serial, atom.name, atom.aa_name, atom.chain_id, atom.aa_number, atom.abs_sasa, atom.rel_sasa))
         
         if output_type == "total":
-            print("{:<5} {:>5} {:>4} {:>4} {:>4} {:>4} {:>8.2f} {:>8}".format("TOTAL", "", "", "", "", "", self.protein.total_absolute_sasa, ""))  
-                  
+            print("{:<5} {:>5} {:>4} {:>4} {:>4} {:>4} {:>8.2f} {:>8}".format("TOTAL", "", "", "", "", "", self.protein.total_absolute_sasa, ""))        
         if output_type == "residue":
             chain_sasa = {}
             amino_acid_sasa = {}
